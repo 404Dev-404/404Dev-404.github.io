6f6ec4de604f1df9a50fe3edef37a694b78e6391
404Dev-404 <83556946+404Dev-404@users.noreply.github.com>
2024-04-13 18:58:56 -0600

    some html fixes and a new feature
    the new feature is that when you click the "logo" it displays a random fun fact
    
    submitted fun facts will be appreciated

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
-
-sound = new Audio("sounds/sound1.wav")
+sound = new Audio("sounds/sound1.wav");
 const gifSound = () => sound.play();
+
+let alerts = ["placeholder string leave an issue if you can think of something funny", "test1", "test2", "test3", "javascript gives me kidney stones"];
+
+// javascript gives me kidney stones /j
+// context: post on the twitter account @vibrifacts 
+// which was my profile picture and something like 
+// "mfw i get kidney stones"
+const randomAlert = () => alert("fun fact: " + alerts[Math.floor(Math.random() * alerts.length)]);
